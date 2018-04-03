cb057aefc5ae5c5c3c2c112161eb5e788d7a2f41
Alexander Napolov <anapolov@pierb507p07.engin.umich.edu>
2018-04-03 13:25:38 -0400

    Added neighbors vector

--- src/block.hpp
+++ src/block.hpp
@@ -1,11 +1,13 @@
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
 #include <string>
+#include <vector>
 typedef struct {
 	std::string blockid;
 	int Vdem, Vgop;	
 	double intPtLat, intPtLon;
 	int area;
+	std::vector<Block*> neighbours;
 	int district;
 } Block;
 #endif
