401771ce975bc187bfb52581690bccc9ec1142af
Sasha Napolov <sasha.napolov@gmail.com>
2018-04-13 15:36:58 -0400

    Block->Tract. Added Main, simulated annealing

--- src/tract.hpp
+++ src/tract.hpp
@@ -4,47 +4,47 @@ Uniqname:    woodjosh
 Date:        04/10/2018
 Lab Section: 102
 Project #:   HACK
-Filename:    block.hpp
+Filename:    Tract.hpp
  
 COMPILE WITH: g++ -std=c++11 <FILE> -o <FILEOUT>
 
-Program Description: Assigns information to the block struct. To use it in the program, 
-just do: vector<Block> blocks = getBlockData(); and blocks will contain all of the 
-census blocks in Illinois with their information filled with county wide voting data.
+Program Description: Assigns information to the Tract struct. To use it in the program, 
+just do: vector<Tract> Tracts = getTractData(); and Tracts will contain all of the 
+census Tracts in Illinois with their information filled with county wide voting data.
 Takes about 8 seconds to run through right now.  
   
 *************************************************************************************/
-#ifndef BLOCK_H
-#define BLOCK_H
+#ifndef TRACT_H
+#define TRACT_H
 #include <string>
 #include <vector>
 
 using namespace std;
 
 typedef struct {
-	string blockid;
+	string Tractid;
 	double Vdem, Vgop;	
 	double intPtLat, intPtLon;
 	int area;
-	vector<int> neighbours; //Indices of the neighboring blocks
+	vector<int> neighbours; //Indices of the neighboring Tracts
 	string countyId;
 	string countyName;  
 	string districtId;
 	string districtName; 
 	int population;
 	int district;
-} Block;
+} Tract;
 
 typedef struct {
 	string districtId;
 	string districtName;
 	string countyId;
 	string countyName;  
-	int blockCount; 	
+	int TractCount; 	
 } nameIdCorr;
 
-//Returns a vector of blocks with all of the filled in information. 
-vector<Block> getBlockData();
+//Returns a vector of Tracts with all of the filled in information. 
+vector<Tract> getTractData();
 //Adds county names and ids to a struct made for reading from the txt file
 vector<nameIdCorr> countyNameId();
 //adds county name, dem votes and rep votes to a vector of vectors of strings
@@ -55,8 +55,8 @@ int strpos(string haystack, char needle, int nth);
 void displayNameIdCorr(vector<nameIdCorr> nameId, int n);
 //displays voteData at index n 
 void displayVoteData(vector<vector<string> > vote, int n);
-//displays block at index n
-void displayBlock(vector<Block>, int n);
+//displays Tract at index n
+void displayTract(vector<Tract>, int n);
 
 //these are not currently used, but were attempts for using precinct level data
 vector<nameIdCorr> districtNameId();
