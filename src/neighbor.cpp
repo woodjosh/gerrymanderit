561a32ba6ada02be525ae026333a2bc186a3b048
Yuxi Xie <yuxixie@Yuxis-MacBook-Air.local>
2018-04-15 17:46:06 -0400

    uploaded rook neighbors

--- src/neighbor.cpp
+++ src/neighbor.cpp
@@ -13,22 +13,22 @@
 #include <sstream>
 #include <vector>
 #include <stdlib.h>
-#include <block.hpp>
+#include <tract.hpp>
 using namespace std;
 
 
-void inputNeighbor(vector<Block> &block, vector < vector<string> > neighbors, vector<string> blockid)
+void inputNeighbor(vector<Tract> &tract, vector < vector<string> > neighbors, vector<string> tractid)
 {
     vector<int> neighborIndex;
-    int blocknum = (unsigned)block.size();
-    for(int k = 0; k<blocknum; k++)
+    int tractnum = (unsigned)tract.size();
+    for(int k = 0; k<tractnum; k++)
     {
     neighborIndex.clear();
-    int idIndex = 0;// for the block
-    string findblockid = block[k].blockid;
-    for (int i = 0; i<blocknum;i++)
+    int idIndex = 0;// for the tract
+    string findtractid = tract[k].tractid;
+    for (int i = 0; i<tractnum;i++)
     {
-        if(blockid[i] == findblockid)
+        if(tractid[i] == findtractid)
         {
             idIndex = i;
             break;
@@ -38,9 +38,9 @@ void inputNeighbor(vector<Block> &block, vector < vector<string> > neighbors, ve
     int sz = (unsigned)neighbors[idIndex].size();
     for(int i = 0; i<sz; i++)
     {
-        for(int j = 0; j<blocknum;j++)
+        for(int j = 0; j<tractnum;j++)
         {
-            if(blockid[j] == neighbors[idIndex][i])
+            if(tractid[j] == neighbors[idIndex][i])
             {
                 neighborIndex.push_back(j);
                 break;
@@ -48,41 +48,73 @@ void inputNeighbor(vector<Block> &block, vector < vector<string> > neighbors, ve
         }
     }
     
-    block[k].neighbours = neighborIndex;// should be index of neighbours
+    tract[k].neighbours = neighborIndex;// should be index of neighbours
     }
 }
     
 
-vector<string> blockid(451600); // in main()
+vector<string> tractid(451600); // in main()
 vector<string> neighborId(100); //in main()
 vector < vector<string> > neighbors(451600, neighborId);// in main()
 
-void readneighbor(vector<string> &blockid, vector< vector <string> > &neighbors) {
-    vector<int> numOfNeighbor(451600);
+void readneighbor(vector<string> &tractid, vector< vector <string> > &neighbors) {
+    vector<int> numOfNeighbor(3500);
     string in;
     string ids;
     
     ifstream queenfile;
-    queenfile.open("rook.gal");
+    queenfile.open("rookTract.gal");
     int i = 0;
     getline(queenfile,in);
     
     while(!queenfile.fail())
     {
-        blockid[i]=in.substr(0,15);
-        numOfNeighbor[i]=stod(in.substr(16,2));
+        tractid[i]=in.substr(9,11);
+        numOfNeighbor[i]=stod(in.substr(21,2));
         //cout<<numOfNeighbor[i]<<endl;
         getline(queenfile, ids);
         int j = 0;
         while(j<numOfNeighbor[i])
         {
-            string sub = ids.substr(j*16,15);
-            neighbors[i][j] = sub;
+            string sub = ids.substr(j*21,20);
+            neighbors[i][j] = sub.substr(9,11);
             //cout<<neighbors[i][j]<<endl;
             j++;
         }
         i++;
         getline(queenfile,in);
+    }
+    
+    
+    void inputArea(vector<Tract> &tract)
+    {
+        int sz = (unsigned) tract.size();
+        vector<string> state(3500);
+        vector<string> geoid(3500);
+        vector<string> aland(3500);
+        vector<string> awater(3500);
+        vector<string> aland_sqmi(3500);
+        vector<string> awater_sqmi(3500);
+        vector<string> lat(3500);
+        vector<string> lon(3500);
+        
+        ifstream file("area.rtf");
+        int i = 0;
+        file>>state[i]>>geoid[i]>>aland[i]>>awater[i]>>aland_sqmi[i]>>awater_sqmi[i]>>lat[i]>>lon[i];
+        while (!file.fail())
+        {
+            for(int j = 0; j<sz; j++)
+            {
+                if(tract[j].tractid == geoid[i])
+                {
+                    double area_sqmi = stod(aland_sqmi[i]) + stod(awater_sqmi[i]);
+                    tract[j].area = area_sqmi;
+                    break;
+                }
+            }
+            i++;
+            file>>state[i]>>geoid[i]>>aland[i]>>awater[i]>>aland_sqmi[i]>>awater_sqmi[i]>>lat[i]>>lon[i];
+        }
     }
     
 }
