e8d8c9a11021a23ec5813be3feaf95cf87a52546
sasha-napolov <sasha.napolov@gmail.com>
2018-04-15 19:26:33 -0400

    Create neighbor.hpp

---
+++ src/neighbor.hpp
@@ -0,0 +1,10 @@
+#ifndef NEIGHBOR_H
+#define NEIGHBOR_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+void inputNeighbor(vector<Tract> &tract, vector < vector<string> > neighbors, vector<string> tractid);
+void readneighbor(vector<string> &tractid, vector< vector <string> > &neighbors);
+#endif
